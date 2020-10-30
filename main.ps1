8c5b194e1cb52bd4bd6f8701dc2bb3902cf7d1cf
Gustav Andersson <46231271+FanatiQS@users.noreply.github.com>
2020-10-30 19:23:06 +0100

    Initial commit

---
+++ main.ps1
@@ -0,0 +1,12 @@
+# Sets current-working-directory to parent of executed file
+Set-Location $PSScriptRoot
+
+# Downloads deno executable to cwd if it does not exist
+if (!(Test-Path ./bin/deno.exe)) {
+	$env:DENO_INSTALL = "."
+	iwr https://deno.land/x/install/install.ps1 -useb | iex
+}
+
+# Runs deno script ininately with executalbe and cache in cwd
+$env:DENO_DIR = "."
+& ./bin/deno run https://raw.githubusercontent.com/beeman/deno-examples/master/apps/hello-world.ts
